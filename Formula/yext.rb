78f3f358ae021b16ca83b3d7d4b56dd2795ae8d8
Aditya Saurabh <asaurabh@yext.com>
2019-10-04 15:00:57 -0400

    sync sha

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -1,39 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# This formula is for installing the Yext command-line interface
 class Yext < Formula
-  desc "Yext Command Line Interface"
-  homepage "www.yext.com"
+  desc "Command-line Interface for Yext Platform"
+  homepage "http://www.yext.com"
   url "https://yext-cli-dev.s3.amazonaws.com/cli/mac/yext"
   version "0.1"
   sha256 "e3511b0295cd290025d925ef46dc00bfb457fc9c5c887b79b33012d2de9987b5"
-
-  # depends_on "cmake" => :build
+  revision 1
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
     bin.install "yext"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test yext`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/yext --help"
-    # build-yext
-    # input.yext
-#    system "false"
+    system "#{bin}/yext", "--help"
   end
 end
