f916ae5ac1d2c45e12cd56a1bbf3c49b37df6ae1
Aditya Saurabh <asaurabh@yext.com>
2019-10-07 19:17:15 -0400

    sync sha at

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -4,7 +4,7 @@ class Yext < Formula
   homepage "http://www.yext.com"
   url "https://yext-cli-dev.s3.amazonaws.com/cli/mac/yext"
   version "0.1"
-  sha256 "e3511b0295cd290025d925ef46dc00bfb457fc9c5c887b79b33012d2de9987b5"
+  sha256 "8de2e72e95e8d32a62c98cba11c6b1b3c8d9991e56b0b6a7185c7d906edb811f"
   revision 1
 
   def install
