0ad31683b9ead87dfd365bb9e867cc488c1ad68b
CI <noreply+CI@yext.com>
2019-10-08 13:51:24 -0400

    flip to use yext-cli-pub

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -2,7 +2,7 @@
 class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
-  url "https://yext-cli-dev.s3.amazonaws.com/cli/mac/yext"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext"
   version "0.1"
   sha256 "8de2e72e95e8d32a62c98cba11c6b1b3c8d9991e56b0b6a7185c7d906edb811f"
   revision 2
