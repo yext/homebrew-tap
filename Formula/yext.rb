105cc21ef886d135a82f0b884a6f65e511b037bc
CI <noreply+CI@yext.com>
2019-10-08 13:44:53 -0400

    up revision

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -5,7 +5,7 @@ class Yext < Formula
   url "https://yext-cli-dev.s3.amazonaws.com/cli/mac/yext"
   version "0.1"
   sha256 "8de2e72e95e8d32a62c98cba11c6b1b3c8d9991e56b0b6a7185c7d906edb811f"
-  revision 1
+  revision 2
 
   def install
     bin.install "yext"
