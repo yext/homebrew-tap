fdb8b4fbd0d05c34d1456513dd07c8824154fc1d
Aditya Saurabh <asaurabh@yext.com>
2019-10-16 17:35:12 -0400

    point to unversioned due to an error when I use brew install
    ==> Downloading https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-55c4a3c932b2647
    ######################################################################## 100.0%
    Error: An exception occurred within a child process:
      Errno::ENOENT: No such file or directory - yext

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -2,7 +2,7 @@
 class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-55c4a3c932b2647c13ab70405cc6a951"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext"
   version "0.1"
   sha256 "b7252c0976a8679f3483d1d26701f54873e4a8964ea1c316fc16476ddd97a3de"
   revision 2
