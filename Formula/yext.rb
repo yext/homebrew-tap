2a32394b2252066f0209e42f9f9439567a258144
Aditya Saurabh <asaurabh@yext.com>
2019-10-16 18:25:47 -0400

    revision based file

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -2,12 +2,13 @@
 class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-55c4a3c932b2647c13ab70405cc6a951"
   version "0.1"
   sha256 "b7252c0976a8679f3483d1d26701f54873e4a8964ea1c316fc16476ddd97a3de"
-  revision 2
+  revision 3
 
   def install
+    mv "yext-55c4a3c932b2647c13ab70405cc6a951", "yext"
     bin.install "yext"
   end
 
