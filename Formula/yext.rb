58fd11aeee176c677a3cf5e2caafd4d7bfa01691
CI <noreply+ci@yext.com>
2019-10-18 22:45:46 -0400

    sync sha at

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -5,7 +5,7 @@ class Yext < Formula
   url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-5d04c75e60e324860f8a345e595c72bb"
   version "0.1"
   sha256 "24eed3185ff6b24abf11d16f304f524af7671947a5f2e6ccfb608583247af375"
-  revision 5
+  revision 6
 
   def install
     mv "yext-5d04c75e60e324860f8a345e595c72bb", "yext"
