458852178fb4876479441833f6eb591030cf647c
Aditya Saurabh <asaurabh@yext.com>
2019-12-10 17:36:37 -0500

    back to 12

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -5,7 +5,7 @@ class Yext < Formula
   url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-b38f5b235c3eae3198cc589c83dd4c72"
   version "0.1"
   sha256 "aedcca891de03fc9b30123a9dedf55507e524bdaa217a56b0ae6dd64ec087c54"
-  revision 13
+  revision 12
 
   def install
     mv "yext-b38f5b235c3eae3198cc589c83dd4c72", "yext"
