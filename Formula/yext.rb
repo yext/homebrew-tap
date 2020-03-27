11c014ba09b534553c9ff31419062bad1c3f07e4
CI <asaurabh@yext.com>
2020-03-27 17:08:58 -0400

    Revert "sync sha at 4292853"
    This reverts commit c4a5745c183c927a0cd1c140e9c2114c65e64743.

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -3,13 +3,13 @@ class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-c1a6c0652f259e7e2d3ebe2a63661d58"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-cbf851aba770e48046320c396b0e4a3c"
   version "0.1"
-  sha256 "6435d64a136cb6885339686b250742016231eb81e5e6ba281561b05b40b6b5f5"
-  revision 32
+  sha256 "3e0d048792398380c3ef1761c5d5437e5b76858de9cd95a81f72315a383a1212"
+  revision 31
 
   def install
-    mv "yext-c1a6c0652f259e7e2d3ebe2a63661d58", "yext"
+    mv "yext-cbf851aba770e48046320c396b0e4a3c", "yext"
     bin.install "yext"
   end
 
