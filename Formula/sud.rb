019938532822490c30993cedc9f69cf726585d8c
CI <noreply+ci@yext.com>
2020-07-10 16:42:33 -0400

    sync sha for sud at 5597398

--- Formula/sud.rb
+++ Formula/sud.rb
@@ -3,13 +3,13 @@ class Sud < Formula
   desc "Command-line Interface for sud"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://yext-cli-pub.s3.amazonaws.com/sud/mac/sud-33081d87bc6adf06de0c5f9c12a718a3"
+  url "https://yext-cli-pub.s3.amazonaws.com/sud/mac/sud-0c4f108165bb7d9d6ba0d4ad1f3350e7"
   version "0.1"
-  sha256 "f1f6a4ab967bd19de91045118e0ac6fa225391a2e5200c091b663ba29eaf658c"
-  revision 4
+  sha256 "adace932e91ac5f05b2209214a3534ec26414fbcd0b15895943a6aa8e964bc76"
+  revision 5
 
   def install
-    mv "sud-33081d87bc6adf06de0c5f9c12a718a3", "sud"
+    mv "sud-0c4f108165bb7d9d6ba0d4ad1f3350e7", "sud"
     bin.install "sud"
   end
 
