fa08a50acd0c99703f336b4b176b20019446dcb4
runner <runner@Mac-1597075043366.local>
2020-08-10 16:02:23 +0000

    sud 0.2

--- Formula/sud.rb
+++ Formula/sud.rb
@@ -3,10 +3,9 @@ class Sud < Formula
   desc "Command-line Interface for sud"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://yext-cli-pub.s3.amazonaws.com/sud/mac/sud-b70a292811c9d5561cf8280c5df5a2f3"
-  version "0.1"
-  sha256 "9abca20c02e2c92e4fb06217c29e7cde5ff8e891352563874b6aad50abc544ad"
-  revision 8
+  url "https://github.com/yext/sud/releases/download/0.2/sud-5c15b463b8e014518566c9689eb2dada41737db2ab0aad7fc4dc303014bc591f"
+  version "0.2"
+  sha256 "5c15b463b8e014518566c9689eb2dada41737db2ab0aad7fc4dc303014bc591f"
 
   def install
     mv "sud-b70a292811c9d5561cf8280c5df5a2f3", "sud"
