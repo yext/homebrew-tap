4c32560b80a817ac448efc748e47dfe5b3ec048e
Aditya Saurabh <saurabh.aditya@gmail.com>
2020-08-12 12:09:55 -0400

    version from 0.1 to v0.1

--- Formula/sud.rb
+++ Formula/sud.rb
@@ -4,7 +4,8 @@ class Sud < Formula
   homepage "http://www.yext.com"
   depends_on "git"
   url "https://github.com/yext/sud/releases/download/0.3/sud-f27731f3df542eff42b4029c1f22db314715037f282faa043a20f49a0ff49a09"
-  version "0.3"
+  version "v0.1"
+  revision 8
   sha256 "f27731f3df542eff42b4029c1f22db314715037f282faa043a20f49a0ff49a09"
   def install
     mv "sud-f27731f3df542eff42b4029c1f22db314715037f282faa043a20f49a0ff49a09", "sud"
