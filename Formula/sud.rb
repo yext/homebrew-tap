b894c9ba59e97c01266cb142e7ab7c0ca37d57cd
Aditya Saurabh <saurabh.aditya@gmail.com>
2020-08-12 14:06:17 -0400

    sud 0.1_19
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/sud.rb
+++ Formula/sud.rb
@@ -3,10 +3,10 @@ class Sud < Formula
   desc "Command-line Interface for sud"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://github.com/yext/sud/releases/download/v0.1_17/sud-7b2eaac08884fd41a099cd8d55dde1a70d421dd4d7bfe8c192d08bbaa51cc4f0"
-  version "v0.1"
+  url "https://github.com/yext/sud/archive/v0.1_19.tar.gz"
+  version "0.1_19"
   revision 9
-  sha256 "7b2eaac08884fd41a099cd8d55dde1a70d421dd4d7bfe8c192d08bbaa51cc4f0"
+  sha256 "5d21b39126ac7ffaa6e9fd32d43b76660b93b212f7f6a04360a123871f3874d2"
   def install
     mv "sud-7b2eaac08884fd41a099cd8d55dde1a70d421dd4d7bfe8c192d08bbaa51cc4f0", "sud"
     bin.install "sud"
