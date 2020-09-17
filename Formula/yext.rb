bea3acd1426ce556e98fa7d48a5ab415a90617c7
CI <noreply+ci@yext.com>
2020-09-17 15:18:39 +0000

    sync sha at 6696042

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -3,13 +3,13 @@ class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-b44d356be5cd25f499ac1f2551e8754c"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-618c62d03494af531e4d07bc124533f7"
   version "0.1"
-  sha256 "dfb7ae82e532efb41ee8a8d7605dbd677f905b9e7029281a6a214311c7cf0e0e"
-  revision 42
+  sha256 "6f3ffed7f262456ea69054a4aed39fc52ab306ad0e75ce7ddb32bb5d576c4cb8"
+  revision 43
 
   def install
-    mv "yext-b44d356be5cd25f499ac1f2551e8754c", "yext"
+    mv "yext-618c62d03494af531e4d07bc124533f7", "yext"
     bin.install "yext"
   end
 
