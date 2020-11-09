16c8cd410cfe075165267a41ce3e6e440bf279ce
Ethan Cohen <ecohen@yext.com>
2020-11-09 15:00:56 -0500

    Yext: Add recommended bash-completion dependency
    This is needed to use the bash autocompletion. Marking as recommended
    instead of required as users not using bash may wish to opt out

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -3,6 +3,7 @@ class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
   depends_on "git"
+  depends_on "bash_completion" => :recommended
   url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-b859e6c77d78587fa4a2a56c0c002e7b"
   version "0.1"
   sha256 "e362c07efe7442c250877e531ccbc65df2aefd9fc6216f2c1a01df4ac454bb46"
