6b1f049a0baefe1e4e03eceac3ce45487452dd38
Aditya Saurabh <saurabh.aditya@gmail.com>
2020-11-09 08:41:32 -0500

    brew info testpath/info.out should create a file
    Update yext.rb - add a test that brew info testpath/info.out should create a file

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -3,17 +3,20 @@ class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
   depends_on "git"
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-309dcd7a511e88b72ce9d6882b96fd74"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-b859e6c77d78587fa4a2a56c0c002e7b"
   version "0.1"
-  sha256 "063d4a317e127c720ce0ca7a40189ff8b1dc5afbe47e9dc90932564c5a631895"
-  revision 55
+  sha256 "e362c07efe7442c250877e531ccbc65df2aefd9fc6216f2c1a01df4ac454bb46"
+  revision 51
 
   def install
-    mv "yext-309dcd7a511e88b72ce9d6882b96fd74", "yext"
+    mv "yext-b859e6c77d78587fa4a2a56c0c002e7b", "yext"
     bin.install "yext"
   end
 
   test do
     system "#{bin}/yext", "--help"
+    infout = testpath/"info.out"
+    system "#{bin}/yext", "info", infout
+    assert_predicate infout, :exist?    
   end
 end
