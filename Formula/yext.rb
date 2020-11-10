048593a91742af878b791d730898f6c37a252d1e
Aditya Saurabh <saurabh.aditya@gmail.com>
2020-11-10 10:17:33 -0500

    change to bash-completion

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -3,7 +3,7 @@ class Yext < Formula
   desc "Command-line Interface for Yext Platform"
   homepage "http://www.yext.com"
   depends_on "git"
-  depends_on "bash_completion" => :recommended
+  depends_on "bash-completion" => :recommended
   url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-6f6767683e313ac8616f8e03ef1b2712"
   version "0.1"
   sha256 "b297700e85023997a515defc5f3547e7416d0ce8b3ec5499a7e9ec35befe9ca9"
