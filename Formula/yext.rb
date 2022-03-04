aaf88f9ecf760ab5b4fc0e7a0fc5b7e627bf7f99
CI <noreply+ci@yext.com>
2022-03-04 18:22:47 +0000

    sync sha at 19374916

--- Formula/yext.rb
+++ Formula/yext.rb
@@ -4,13 +4,13 @@ class Yext < Formula
   homepage "http://www.yext.com"
   depends_on "git"
   depends_on "bash-completion" => :recommended
-  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-5ebc030842c936876464b1bf7fc56d53"
+  url "https://yext-cli-pub.s3.amazonaws.com/cli/mac/yext-be124a194354284bd4594f305c94b0e0"
   version "0.1"
-  sha256 "a6bdd5178f956aee0b04047b15c2eae9a05083bebeab3a7beb98ce5f6c6d68e0"
-  revision 236
+  sha256 "a9e36523bb2435be0a0cf9de906ff20ab8edbfccb382e8b40c1d1bdfea010cb5"
+  revision 237
 
   def install
-    mv "yext-5ebc030842c936876464b1bf7fc56d53", "yext"
+    mv "yext-be124a194354284bd4594f305c94b0e0", "yext"
     bin.install "yext"
 
     # s3 doesnt retain file permissions, so have to do this to add autocomplete
