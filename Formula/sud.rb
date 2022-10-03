96381cf8338fccab9aa3327491b78141f5afa65d
Aditya Saurabh <asaurabh@yext.com>
2022-10-03 11:49:00 -0400

    update sha256 of the mv line

--- Formula/sud.rb
+++ Formula/sud.rb
@@ -7,7 +7,7 @@ class Sud < Formula
   version "0.1_27"
   sha256 "ee8f8f6c3e00f124977303ca88c028bd789b9233e9c8cae5b6d14269ab7ed64a"
   def install
-    mv "sud-ad8a666f766bf0d7f8d07975663035181f8a6e8554a5d93e31ca50d09ced4159", "sud"
+    mv "sud-ee8f8f6c3e00f124977303ca88c028bd789b9233e9c8cae5b6d14269ab7ed64a", "sud"
     bin.install "sud"
   end
 
